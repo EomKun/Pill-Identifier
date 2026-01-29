c8c61773d04d46c41a8a96bf031b8dd82bfd78e9
JaeWoong Eom <zzang6535@gmail.com>
2026-01-29 16:12:30 +0900

    feat: 객체 탐지 로직 수정 및 재학습용 이미지 저장 기능 구현 (#1)
    * Initial commit
    
    * first commit
    
    * 👔Add Detection logic
    
    모델 기반으로 객체 탐지 로직 추가
    
    * 👔 update detection logic
    
    - 탐지 로직 수정, 엔드포인트 정의
    - 모델 테스트(65퍼로 저조해서 추가 학습 필요)
    
    * 👔 modify detection logic
    
    - 객체 탐지시 재 학습을 위한  이미지 저장 로직 추가

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,2 +1,35 @@
 # 비즈니스 로직 호출
 # 엔드 포인트를 나눠야 할 필요가 있는지? 고민
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from app.services.detection import DetectionService
+
+router = APIRouter()
+detector = DetectionService("app/models/best.pt")
+
+@router.post("/pills/detect")
+async def detect_pills(file: UploadFile = File(...)):
+    """객체 탐지 확인용 임시 엔드포인트
+
+    Args:
+        file(UploadFile): 요청에 포함된 파일(이미지)
+
+    Returns:
+        json 응답 (status, pill_count, message)
+    """
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="이미지 파일만 업로드 가능합니다.")
+
+    try:
+        # 이미지 바이트 읽기
+        image_bytes = await file.read()
+
+        saved_pill_paths = detector.detection_process(image_bytes)
+
+        # 현재는 크롭된 이미지 객체 리스트이므로, 개수와 간단한 메시지만 반환
+        return {
+            "count": len(saved_pill_paths),
+            "pill_locations": saved_pill_paths  # ["data/temp_crops/2024..._0.jpg", ...]
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"서버 오류: {str(e)}")
